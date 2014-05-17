676d52d5623bc81dfacb35cc84671fdf19971f5c
Nicki Ogg <oggnicki@yahoo.com>
2014-05-16 22:39:25 -0400

    faster prob 9

--- euler9.rb
+++ euler9.rb
@@ -1,16 +1,14 @@
-for k in 1000.downto(1)
-	for i in 1..k
-		for j in i..k
-			if (i**2 + j**2 > k**2)
-				break
-			end
-			if (i**2 + j**2 == k**2)
-				if (i + j + k == 1000)
-					puts "#{i} #{j} #{k}"
-					puts "product: #{i*j*k}"
-					exit
-				end
-			end
+sum = 1000
+
+for i in 1..sum
+	for j in i..sum
+		if (i**2 + j**2 > (sum-i-j)**2)
+			break
+		end
+		if (i**2 + j**2 == (sum-i-j)**2)
+			puts "#{i} #{j} #{(sum-i-j)}"
+			puts "product: #{i*j*(sum-i-j)}"
+			exit
 		end
 	end
 end
