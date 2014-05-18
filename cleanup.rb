f316dddd82fcab2fee1570715df0954458d3d52f
Nicki Ogg <oggnicki@yahoo.com>
2014-05-17 22:42:54 -0400

    Added comments

--- cleanup.rb
+++ cleanup.rb
@@ -6,8 +6,10 @@ require 'fileutils'
 files = Dir["*"]
 
 files.map do |name|
+	#this matches names starting with a euler[number].
 	match = /euler(\d+)\..+/.match(name)
 	if (match)
+		#the number is stored in MatchData[1]
 		number = match[1]
 		path = "Problem " + number + "/" + name
 		FileUtils.mkdir_p(File.dirname(path))
