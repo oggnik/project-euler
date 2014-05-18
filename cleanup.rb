69234a002ca765d5573e4b1cea02e8b8830d5ae6
Nicki Ogg <oggnicki@yahoo.com>
2014-05-17 23:08:47 -0400

    Cleaner regex

--- cleanup.rb
+++ cleanup.rb
@@ -7,10 +7,10 @@ files = Dir["*"]
 
 files.map do |name|
 	#this matches names starting with a euler[number].
-	match = /euler(\d+)\..+/.match(name)
+	match = /euler(?<number>\d+)\..+/.match(name)
 	if (match)
-		#the number is stored in MatchData[1]
-		number = match[1]
+		#the number is stored in MatchData[:number]
+		number = match[:number]
 		path = "Problem " + number + "/" + name
 		FileUtils.mkdir_p(File.dirname(path))
 		FileUtils.mv(name, path)
